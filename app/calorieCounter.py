766532b0acaa2b127570d5bfdf961d603463675e
Magdalena <magdawnowicka@gmail.com>
2022-05-27 14:10:51 +0200

    added calories counting and displaying

--- app/calorieCounter.py
+++ app/calorieCounter.py
@@ -1,17 +1,57 @@
-from searchService import SearchService
+from app.models import Ingredient
 
-api = SearchService()
-def calcNutrientsForEntry(foodId, servingId, quantity):
-    food = api.getFoodById(foodId)
-    myServing = None
-    for serv in food['servings']['serving']:
-        if serv['serving_id'] == servingId:
-            myServing = serv
-            return (float(myServing['calories']) * quantity, float(myServing['protein']) * quantity, float(myServing['fat']) * quantity, float(myServing['carbohydrate']) * quantity)
+class CalorieCounter:
 
-def addIngredientToMeal(mealId, foodId, servingId, quantity):
-    pass
+    def __init__(self, api) :
+        self.__api = api
 
+    def calcNutrientsForServing(self, foodid, servingid, quantity):
+        result = self.__api.getFoodById(foodid)
+        myServing = None
+        servings = result['servings']['serving']
+        if type(servings) != list:
+            servings = [servings]
+        for serv in servings:
+            if serv['serving_id'] == servingid:
+                myServing = serv
+                return {'calories': round(float(myServing['calories']) * quantity), "protein":float(myServing['protein']) * quantity,
+                    "fat": float(myServing['fat']) * quantity, "carbohydrate": float(myServing['carbohydrate']) * quantity}
 
-print("\nkcal, protein, fat, carbs")
-print(calcNutrientsForEntry(794, '733', 1))
+    def calcNutrientsForFood(self, food):
+        result = self.__api.getFoodById(food.food_id)
+        myServing = None
+        servings = result['servings']['serving']
+        if type(servings) != list:
+            servings = [servings]
+        for serv in servings:
+            if serv['serving_id'] == food.serving_id:
+                myServing = serv
+                return {'calories' :float(myServing['calories']) * food.quantity, "protein":float(myServing['protein']) * food.quantity,
+                    "fat": float(myServing['fat']) * food.quantity, "carbohydrate": float(myServing['carbohydrate']) * food.quantity}
+
+    def calcNutrientsForMeal(self, foodList):
+        meal_nutrients={"calories": 0, "protein": 0, "fat": 0, "carbohydrate": 0}
+        foods_nutr ={}
+        for food in foodList:
+            nutrients = self.calcNutrientsForFood(food)
+            foods_nutr[food.food_id] = nutrients
+            meal_nutrients['calories'] += nutrients['calories']
+            meal_nutrients['protein'] += nutrients['protein']
+            meal_nutrients['fat'] += nutrients['fat']
+            meal_nutrients['carbohydrate'] += nutrients['carbohydrate']
+        return {'meal_sum': meal_nutrients, 'foods_sum': foods_nutr}
+
+    def calcNutrientsForDay(self, mealList):
+        day_nutrients={"calories": 0, "protein": 0, "fat": 0, "carbohydrate": 0}
+        meals_nutrients={}
+        for meal in mealList:
+            ingredients = Ingredient.query.filter_by(meal=meal.id).all()
+            meal_nutr = self.calcNutrientsForMeal(ingredients)
+            meals_nutrients[meal.type] = meal_nutr['meal_sum']
+
+            day_nutrients['calories'] += meal_nutr['meal_sum']['calories']
+            day_nutrients['protein'] += meal_nutr['meal_sum']['protein']
+            day_nutrients['fat'] += meal_nutr['meal_sum']['fat']
+            day_nutrients['carbohydrate'] += meal_nutr['meal_sum']['carbohydrate']
+        return {"day_sum": day_nutrients, "meals_sum": meals_nutrients}
+        
--- app/diet_tracker.py
+++ app/diet_tracker.py
@@ -1,12 +1,17 @@
 import datetime
 import json
 from turtle import update
+from unittest import result
+
+from app import calorieCounter
 from .searchService import api
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 from .models import Meal, Ingredient
 from datetime import date
 from .__init__ import db, create_app
+
+calCounter = calorieCounter.CalorieCounter(api)
 
 diet_tracker = Blueprint('diet_tracker', __name__)
 
@@ -45,6 +50,20 @@ def pickFoodDetails(mealid, date, foodid):
         quantity = None
 
     return render_template('foodDetails.html',mealid=mealid, mealtype=mealtype, date=date, food=result, serving=serving, quantity=quantity, update=update)
+
+@diet_tracker.route('/diet-tracker/preview-calories')
+@login_required
+def countPreviewCalories():
+    foodid = request.args.get('foodid')
+    servingid = request.args.get('servingid')
+    quantity = request.args.get('quantity')
+    print(foodid, servingid, quantity)
+
+    result = calCounter.calcNutrientsForServing(foodid, servingid, float(quantity))
+    print(result)
+    return json.dumps(result)
+
+
 
 @diet_tracker.route('/diet-tracker/addFood', methods=['POST'])
 @login_required
@@ -105,8 +124,14 @@ def searchFoodId(foodId):
 
 @diet_tracker.route('/newMeal/<date>')
 def newMeal(date):
-    # date = date.split("-")
-    return render_template("mealTypeChoice.html", date=date)
+    date_list=date.split("-")
+    meals = Meal.query.filter_by(user=current_user.id, date=datetime.date(int(date_list[0]),int(date_list[1]),int(date_list[2]))).all()
+    mealtypes = ["Breakfast", "Lunch", "Dinner", "Others"]
+    existing = []
+    for meal in meals:
+        existing.append(meal.type)
+        
+    return render_template("mealTypeChoice.html", date=date, existing=existing, mealtypes=mealtypes)
 
 @diet_tracker.route('/diet-tracker/searchIngredients', methods=['POST'])
 def searchIngredients():
@@ -119,33 +144,37 @@ def searchIngredients():
 
 
 @diet_tracker.route('/addMeal/<date>')
-def addMeal(date):    
-    date = date.split("-")
+def addMeal(date):
+    date_list = date.split("-")
     mealtype = request.args.get('type')
-    meal = Meal.query.filter_by(user=current_user.id, date=datetime.date(int(date[0]),int(date[1]),int(date[2])), type=mealtype).first()
+    update = request.args.get('update')
+    meal = Meal.query.filter_by(user=current_user.id, date=datetime.date(int(date_list[0]),int(date_list[1]),int(date_list[2])), type=mealtype).first()
 
     # check if the meal of given type already exists
     if meal:
         flash("Cannot add two meals of the same type")
-        return redirect(url_for("diet_tracker.newMeal", date=datetime.date(int(date[0]),int(date[1]),int(date[2]))))
+        return redirect(url_for("diet_tracker.newMeal", date=datetime.date(int(date_list[0]),int(date_list[1]),int(date_list[2]))))
 
-    new_meal = Meal(type=mealtype, date=datetime.date(int(date[0]),int(date[1]),int(date[2])), user=current_user.id)
+    new_meal = Meal(type=mealtype, date=datetime.date(int(date_list[0]),int(date_list[1]),int(date_list[2])), user=current_user.id)
     db.session.add(new_meal)
     db.session.commit()
     
-    return render_template("foodSearcher.html", foods=None, date=new_meal.date, mealtype=mealtype, mealid=new_meal.id)
+    return render_template("foodSearcher.html", foods=None, date=new_meal.date, mealtype=mealtype, mealid=new_meal.id, update=update)
 
 
-@diet_tracker.route('/diet-tracker/myDay', methods=["POST"])
+@diet_tracker.route('/diet-tracker/myDay', methods=["GET"])
 @login_required
 def getMealsForDay():
-    date = request.form.get("date").split("-")
-    meals = Meal.query.filter_by(user=current_user.id, date=datetime.date(int(date[0]),int(date[1]),int(date[2]))).all()
-    meal_list =[]
-    for meal in meals:
-        meal_list.append({'id' : meal.id, 'type' : meal.type})
-    print(meal_list)
-    return render_template('dayView.html', date=datetime.date(int(date[0]),int(date[1]),int(date[2])), meals=meals)
+    mealOrder = {"Breakfast": 1, "Lunch": 2, "Dinner": 3, "Others": 4}
+    date = request.args.get('date')
+    date_list = date.split("-")
+
+    meals = Meal.query.filter_by(user=current_user.id, date=datetime.date(int(date_list[0]),int(date_list[1]),int(date_list[2]))).all()
+    meals.sort(key=lambda meal: mealOrder[meal.type])
+
+    nutrients = calCounter.calcNutrientsForDay(meals)
+
+    return render_template('dayView.html', date=date, meals=meals, day_nutr=nutrients['day_sum'], meals_nutr=nutrients['meals_sum'])
 
 @diet_tracker.route('/getIngredientsForMeal/<mealid>/<date>')
 @login_required
@@ -162,8 +191,10 @@ def getIngredientsForMeal(mealid, date):
             if s['serving_id'] == ing.serving_id:
                 s_name = s['serving_description']
         food_list.append({'foodid' : ing.food_id, 'name': food_details['food_name'], 'servingid' : ing.serving_id, 's_name':s_name, 'quantity' : ing.quantity})
-    # print(food_list)
-    return render_template('mealView.html', date=date, mealid=mealid, foods=food_list, mealtype=meal.type)
+
+    nutrients = calCounter.calcNutrientsForMeal(ingredients)
+
+    return render_template('mealView.html', date=date, mealid=mealid, foods=food_list, mealtype=meal.type, meal_nutr=nutrients['meal_sum'], foods_nutr=nutrients['foods_sum'])
 
 @diet_tracker.route('/diet-tracker/deleteIngredient')
 @login_required
@@ -177,15 +208,25 @@ def deleteIngredient():
 
     return redirect(url_for("diet_tracker.getIngredientsForMeal", mealid=mealid, date=date))
 
+@diet_tracker.route('/diet-tracker/confirm-delete-meal')
+@login_required
+def confirmDeleteMealOfId():
+    mealtype = request.args.get('mealtype')
+    mealid = request.args.get('mealid')
+    date = request.args.get('date')
 
+    return render_template('confirmDeleteWindow.html',mealid=mealid, mealtype=mealtype, date=date)
 
-def deleteMealOfId(id):
-    app = create_app()
-    with app.app_context():
-        meal = Meal.query.filter_by(id=id).delete()
-        ingredients = Ingredient.query.filter_by(meal=id).delete()
-        db.session.commit()
-        return meal, ingredients
+@diet_tracker.route('/diet-tracker/deleteMeal/<mealid>')
+@login_required
+def deleteMealOfId(mealid):
+    date = request.args.get('date')
+
+    meal = Meal.query.filter_by(id=mealid).delete()
+    ingredients = Ingredient.query.filter_by(meal=mealid).delete()
+    db.session.commit()
+
+    return redirect(url_for("diet_tracker.getMealsForDay", date=date))
 
 # print(displayMealOfUserAndDate(2, datetime(2020,12,5)))
 # deleteMealOfId(6)
